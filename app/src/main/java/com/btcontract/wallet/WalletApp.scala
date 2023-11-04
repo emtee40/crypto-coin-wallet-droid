eb5411bed8be348a59711c0a4502825b459a617e
Anton Kumaigorodski <akumaigorodski@gmail.com>
2023-11-04 18:16:24 +0200

    UI fixes

--- app/src/main/java/com/btcontract/wallet/WalletApp.scala
+++ app/src/main/java/com/btcontract/wallet/WalletApp.scala
@@ -88,7 +88,7 @@ object WalletApp {
   }
 
   def makeAlive: Unit = {
-    ElectrumWallet.chainHash = Block.TestnetGenesisBlock.hash
+    ElectrumWallet.chainHash = Block.LivenetGenesisBlock.hash
     val miscInterface = new DBInterfaceSQLiteAndroidMisc(app, "misc.db")
 
     miscInterface txWrap {
